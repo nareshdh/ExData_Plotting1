d5d24ba6b2938296a1c58b59cac1eecb4fa71ed6
Naresh Dhaubanjar <aryadhau@gmail.com>
2017-06-30 14:38:21 -0500

    Added plot2.R and plot2.png

---
+++ plot2.R
@@ -0,0 +1,12 @@
+# Download data file
+#****************************************************
+#fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#download.file(fileurl, dest="./data/household_power_consumption.zip")
+
+hpc_data <- read.table(text = grep("^[1,2]/2/2007", readLines("./data/household_power_consumption.txt"), value = TRUE), sep = ";", header = FALSE)
+colnames(hpc_data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+hpc_data$datetime <- strptime(paste(hpc_data$Date, hpc_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+png("plot2.png", width=480, height=480)
+with(hpc_data, plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+dev.off()
